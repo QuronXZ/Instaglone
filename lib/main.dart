66cb479cd0fb41162c36380d1c4e6682416717ea
Shivam Gupta <66762938+Shivam755@users.noreply.github.com>
2022-01-22 22:55:20 +0530

    modifying dependencies and proceeding with firebase connection(incomplete)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import "Material_color_generator.dart";
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase/firebase.dart';
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(const MyApp());
 }
+
+// FirebaseFirestore firestore = FirebaseFirestore.instanceFor(app: insta);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,6 +20,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // String name;
+    // firestore
+    //     .collection('users')
+    //     .doc("GZtGoQTDsgPwSTeoevCx")
+    //     .get()
+    //     .then((DocumentSnapshot doc) {
+    //   if (doc.exists) {
+    //     print('Document exists on the database');
+    //     var data = doc.data();
+    //     print(data.toString());
+    //   }
+    // });
     return MaterialApp(
       title: 'Feed',
       theme: ThemeData(primarySwatch: generateMaterialColor(Colors.white)),
@@ -23,12 +44,17 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Instaglone"),
+      appBar: AppBar(
+        title: Text("Instaglone"),
+      ),
+      body: Center(
+        child: Text(
+          "Hello user",
         ),
-        body: Center(
-            child: Text(
-                "Hello user")) // This trailing comma makes auto-formatting nicer for build methods.
-        );
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [],
+      ),
+    );
   }
 }
