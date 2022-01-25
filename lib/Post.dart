752b9d82ff372417185f2c63dcbcbc0dc5694c2f
AryanMan12 <aryanaryan1212.ag@gmail.com>
2022-01-25 21:54:37 +0530

    converting feed from static to dynamic

--- lib/Post.dart
+++ lib/Post.dart
@@ -1,13 +1,69 @@
+import 'dart:convert';
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Post extends StatefulWidget {
-  const Post({Key? key}) : super(key: key);
+  final snapshot;
+  const Post({Key? key, required this.snapshot}) : super(key: key);
 
   @override
-  _PostState createState() => _PostState();
+  _PostState createState() => _PostState(snapshot);
 }
 
 class _PostState extends State<Post> {
+  final snap;
+  _PostState(this.snap);
+
+// Variables
+  Icon fav_icon = Icon(
+    Icons.favorite_border,
+    size: 32,
+  );
+  bool isLiked = false;
+
+// On like Function
+  void on_like() {
+    setState(() {
+      isLiked = !isLiked;
+      if (isLiked) {
+        fav_icon = Icon(
+          Icons.favorite,
+          size: 32,
+          color: Colors.red,
+        );
+        snap["likedBy"].add("Current User");
+        FirebaseFirestore.instance
+            .collection('Posts')
+            .where("createdOn", isEqualTo: snap["createdOn"])
+            .where("owner", isEqualTo: snap["owner"])
+            .get()
+            .then((value) => FirebaseFirestore.instance
+                .collection('Posts')
+                .doc(value.docs.first.id)
+                .update({"likedBy": snap["likedBy"]}));
+      } else {
+        fav_icon = Icon(
+          Icons.favorite_border,
+          size: 32,
+        );
+        snap["likedBy"].remove("Current User");
+        FirebaseFirestore.instance
+            .collection('Posts')
+            .where("createdOn", isEqualTo: snap["createdOn"])
+            .where("owner", isEqualTo: snap["owner"])
+            .get()
+            .then((value) => FirebaseFirestore.instance
+                .collection('Posts')
+                .doc(value.docs.first.id)
+                .update({"likedBy": snap["likedBy"]}));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -23,7 +79,7 @@ class _PostState extends State<Post> {
                 ),
                 SizedBox(width: 10),
                 Text(
-                  "UserName",
+                  snap["owner"],
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                 )
               ],
@@ -33,20 +89,21 @@ class _PostState extends State<Post> {
           Container(
             height: MediaQuery.of(context).size.width,
             width: MediaQuery.of(context).size.width,
-            color: Colors.amber,
+            child: Image.memory(base64Decode(snap["pic"])),
+            color: Colors.black,
           ),
           Row(
             children: [
               IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.favorite_border,
-                  size: 32,
-                ),
+                onPressed: () => on_like(),
+                icon: fav_icon,
               ),
-              Text(
-                "2034 likes",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+              InkWell(
+                onLongPress: () => {},
+                child: Text(
+                  snap["likedBy"].length.toString() + " likes",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                ),
               ),
             ],
           ),
@@ -54,7 +111,7 @@ class _PostState extends State<Post> {
             children: [
               SizedBox(width: 10),
               Text(
-                "Username",
+                snap["owner"],
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
               ),
             ],
@@ -65,7 +122,7 @@ class _PostState extends State<Post> {
               SizedBox(width: 10),
               Expanded(
                 child: Text(
-                  "Caption: College is love. I love college so much. I always check on it and check on Instagram for its notifications. Name is Jijabai College of Arts. ",
+                  snap["caption"],
                   style: TextStyle(fontSize: 14),
                   maxLines: 15,
                   overflow: TextOverflow.ellipsis,
