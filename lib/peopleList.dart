cc5a747164986235ca022e92c4dd10096ecbdb4c
hrshl13 <harshal26092002@gmail.com>
2022-01-26 12:39:06 +0530

    Commit

---
+++ lib/peopleList.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class listPeople extends StatefulWidget {
+  const listPeople({ Key? key }) : super(key: key);
+
+  @override
+  _listPeopleState createState() => _listPeopleState();
+}
+
+class _listPeopleState extends State<listPeople> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
